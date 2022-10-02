f21e31c36840b24c2b0b51c068d20ca0b3d98670
leequiro <leandroquiroga9514@gmail.com>
2022-10-02 19:30:48 -0300

    new: Has been added new envairoments

--- configuration/envairoment.ts
+++ configuration/envairoment.ts
@@ -1,4 +1,7 @@
 export const envairoment = {
-  PORT: process.env.PORT || '8000',
-  HOST: process.env.HOST || 'localhost',
+  PORT        : process.env.PORT || '8000',
+  HOST        : process.env.HOST || 'localhost',
+  DB          : process.env.DB || '',
+  DB_NAME     : process.env.DB_NAME || '',
+  DB_PASSWORD : process.env.DB_PASSWORD || ''
 };
