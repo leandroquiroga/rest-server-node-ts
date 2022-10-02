b59ae2cfc3ef67ff2b1047fd261b3b190c604a32
leequiro <leandroquiroga9514@gmail.com>
2022-10-02 19:30:12 -0300

    add: Has been added connection with database

--- database/connection.ts
+++ database/connection.ts
@@ -1,6 +1,7 @@
 import { Sequelize } from 'sequelize';
+import { envairoment } from '../configuration/envairoment';
 
-const db = new Sequelize('rest-server-tc', 'root', '123456', {
+const db = new Sequelize(`${envairoment.DB}`, `${envairoment.DB_NAME}`, `${envairoment.DB_PASSWORD}`, {
   host: 'localhost',
   dialect: 'mysql',
   // logging: true
